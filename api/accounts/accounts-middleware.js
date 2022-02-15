8e4482398c94f9e5449c98bf2ce0db52ac464fab
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-15 00:12:06 -0600

    checkAccountNameUnique middleware set up

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,4 +1,5 @@
 const Account = require('./accounts-model')
+const db = require('../../data/db-config')
 exports.checkAccountPayload = (req, res, next) => {
   const error = {status: 400}
   const { name, budget } = req.body
@@ -23,11 +24,20 @@ if (error.message) {
   }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-  
-  console.log('checkAccountNameUnique middleware')
-  next()
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try{
+    const existing = await db('accounts')
+    .where('name', req.body.name.trim())
+    .first()
+
+    if(existing){
+      next ({status: 400, message: 'that name is taken'})
+    } else{
+      next()
+    }
+  } catch (err){
+    next(err)
+  }
 }
 
 exports.checkAccountId =  async (req, res, next) => {
