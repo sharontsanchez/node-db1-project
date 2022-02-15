68d8f0d493cb3dce0363509de152feaf8d08acee
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-14 23:38:38 -0600

    checkAccountId middleware has been updated

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,3 +1,4 @@
+const Account = require('./accounts-model')
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
@@ -13,9 +14,18 @@ exports.checkAccountNameUnique = (req, res, next) => {
   next()
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-  
-  console.log('checkAccountId middleware')
-  next()
+exports.checkAccountId =  async (req, res, next) => {
+  try{
+    const account = await Account.getById(req.params.id)
+    if(!account){
+      next({
+        status: 404, message: 'not found'
+      })
+    } else {
+      req.account = account
+      next()
+    }
+  } catch(err){
+    next(err)
+  }
 }
